690d34f190ad6060ee9191154586020b83eb975e
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2017-10-09 10:02:34 +0100

    added authentication validation

--- routes/users.js
+++ routes/users.js
@@ -90,7 +90,7 @@ router.post('/login',
 router.get('/logout', function(req, res, next){
     req.logout();
     req.flash('success_msg', 'You are logged out');
-    res.redirect('/users/login')
+    res.redirect('/users/login');
 });
 
 module.exports = router;
